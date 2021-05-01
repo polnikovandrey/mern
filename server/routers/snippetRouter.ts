3a7d62afb349bd87c5ff5d238930bbe2e3e97945
Andrey Polnikov <polnikov.andrey@gmail.com>
2021-05-01 18:52:12 +0300

    + request validation + async/await request handling + try/catch block to handle server errors + response with a status

--- server/routers/snippetRouter.ts
+++ server/routers/snippetRouter.ts
@@ -1,12 +1,26 @@
 import express, {response, Router} from 'express';
 import  {Snippet} from '../models/snippetModel';
+import {Document} from "mongoose";
 
 const router: Router = express.Router();
-router.post('/', (request, response) => {
-    const { title, description, code } = request.body;              // deconstructing a body json object
-    const newSnippet = new Snippet({
-        title, description, code
-    });
-    newSnippet.save();
+router.post('/', async (request, response) => {
+    try {
+        const { title, description, code } = request.body;              // deconstructing a body json object
+
+        if (!description && !code) {
+            return response.status(400).json({errorMessage: 'You need to enter at least a description or some code.'});
+        }
+
+        const newSnippet = new Snippet({
+            title, description, code
+        });
+
+        const savedSnippet: Document = await newSnippet.save();
+
+        response.json(savedSnippet);
+    } catch(error) {
+        response.status(500).send();
+    }
+
 });
 module.exports = router;
