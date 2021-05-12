fd1498aa13a7322280e2eb151c00003137de8e06
Andrey Polnikov <polnikov.andrey@gmail.com>
2021-05-12 21:00:17 +0300

    +- cascading component-functions

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,14 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-function Welcome(properties: any) {
-    return <h1>Welcome, {properties.user}</h1>;
+function App(properties: any) {
+    const message: string = properties.message;
+    return (
+        <>
+            <Welcome user="John Doe" message={message}/>
+            <Welcome user="Jane Doe" message={message}/>
+            <Welcome user="Pete Doe" message={message}/>
+        </>
+    );
 }
 
-const toRender = <div>
-    <Welcome user="John Doe"/>
-    <Welcome user="Jane Doe"/>
-    <Welcome user="Peter Doe"/>
-</div>
+function Welcome(properties: any) {
+    return (
+        <>
+            <h1>Welcome, {properties.user}</h1>
+            <p>{properties.message}</p>
+        </>
+    );
+}
 
-ReactDOM.render(toRender, document.getElementById('root'));
+ReactDOM.render(<App message="to the React playground"/>, document.getElementById('root'));
