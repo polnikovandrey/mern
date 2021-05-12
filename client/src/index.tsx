e1b77b10e127c87ddb206ee21d7256470c3fa0fe
Andrey Polnikov <polnikov.andrey@gmail.com>
2021-05-12 22:03:46 +0300

    + conditional rendering

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,18 +4,23 @@ import ReactDOM from "react-dom";
 
 function App() {
 
-    const [clicks, setClicks] = useState(0);
+    const [showMessage, setShowMessage] = useState(false);
 
     function handleClick() {
-        setClicks(clicks + 1);
+        setShowMessage(!showMessage);
     }
 
     return (
         <>
             <button onClick={handleClick}>Click me</button>
-            <p>{clicks}</p>
+            {showMessage && <p>Message is visible</p>}
+            {showMessage ? <Message message="Toggled on"/> : <Message message="Toggled off"/>}
         </>
     );
+}
+
+function Message(properties: any) {
+    return <p>{properties.message}</p>;
 }
 
 ReactDOM.render(<App/>, document.getElementById('root'));
