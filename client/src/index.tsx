52c4711fcbf0736e294cb416c27bfff8969d890c
Andrey Polnikov <polnikov.andrey@gmail.com>
2021-05-20 19:22:31 +0300

    + Clock react component

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,26 +1,30 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import ReactDOM from "react-dom";
-
 
 function App() {
 
-    const [showMessage, setShowMessage] = useState(false);
-
-    function handleClick() {
-        setShowMessage(!showMessage);
-    }
 
     return (
         <>
-            <button onClick={handleClick}>Click me</button>
-            {showMessage && <p>Message is visible</p>}
-            {showMessage ? <Message message="Toggled on"/> : <Message message="Toggled off"/>}
+            <Clock/>
+            <Hello/>
+            <Clock/>
         </>
     );
 }
 
-function Message(properties: any) {
-    return <p>{properties.message}</p>;
+function Clock() {
+    const [time, setTime] = useState(new Date().toLocaleTimeString());
+
+    useEffect(() => {
+        setInterval(() => setTime(new Date().toLocaleTimeString()), 1000);
+    }, []);
+    return <p>It's currently {time}</p>
+
+}
+
+function Hello() {
+    return <h1>Hello</h1>;
 }
 
 ReactDOM.render(<App/>, document.getElementById('root'));
