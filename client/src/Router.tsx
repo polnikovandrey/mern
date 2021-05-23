b106763cc419372b3f83066ed975ca7d5f1b9821
Andrey Polnikov <polnikov.andrey@gmail.com>
2021-05-23 16:49:03 +0300

    Snippet Manager + react-router-dom/Link component usages

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import {BrowserRouter, Route, Switch} from "react-router-dom";
+import Navbar from "./components/misc/Navbar";
 
 function Router(): JSX.Element {
 	return (
 		<BrowserRouter>
+			<Navbar/>
 			<Switch>
 				<Route exact path="/">Homepage</Route>
 				<Route path="/login">Login</Route>
