2194e36e08101e47d29cbbe41419df6d57bc74ca
Andrey Polnikov <polnikov.andrey@gmail.com>
2021-06-07 00:15:07 +0300

    Snippet Manager Server Implementing user authentication ..5

--- server/routers/userRouter.ts
+++ server/routers/userRouter.ts
@@ -61,4 +61,45 @@ router.post("/", async (request, response) => {
 	}
 });
 
+router.post('/login', async (request, response) => {
+	try {
+		const {email, password} = request.body;
+
+		if (!email || !password) {
+			return response.status(400)
+						   .json({
+							   errorMessage: 'Please enter all required fields'
+						   });
+		}
+
+		const existingUser = await User.findOne({ email });
+		if (!existingUser) {
+			return response.status(401)
+						   .json({
+							   errorMessage: 'Wrong email or password'
+						   });
+		}
+
+		const correctPassword: boolean = await bcrypt.compare(password, existingUser.passwordHash);
+		if(!correctPassword) {
+			return response.status(401)
+						   .json({
+							   errorMessage: 'Wrong email or password'
+						   });
+		}
+
+		// See http://jwt.io
+		// See http://passwordgenerator.net
+		const token = jwt.sign({
+			id: existingUser._id
+		}, process.env.JWT_SECRET as Secret);
+
+		response.cookie('token', token, { httpOnly: true })
+				.send();
+
+	} catch(error) {
+		response.status(500).send();
+	}
+});
+
 module.exports = router;
